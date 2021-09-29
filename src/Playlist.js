77ee6116225176e01245d10e8aaa3a9429105eeb
Chizo Nwazuo <chizonwazuo@yahoo.com>
2021-09-29 17:35:26 +0000

    v1 get 'basic' stuff working

--- src/Playlist.js
+++ src/Playlist.js
@@ -1,26 +1,55 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
-import { Cloudinary } from "cloudinary-core";
-import "cloudinary-video-player/dist/cld-video-player.light.min";
-import "cloudinary-video-player/dist/cld-video-player.light.min.css";
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import { Cloudinary } from 'cloudinary-core';
+import 'cloudinary-video-player/dist/cld-video-player.light.min';
+import 'cloudinary-video-player/dist/cld-video-player.light.min.css';
+
+const emojiURLs = {
+  love: 'https://res.cloudinary.com/udemic/image/upload/v1632935667/hackmamba-emoji/love.png',
+  down: 'https://res.cloudinary.com/udemic/image/upload/v1632935647/hackmamba-emoji/down.png',
+  joy: 'https://res.cloudinary.com/udemic/image/upload/v1632935531/hackmamba-emoji/joy.png',
+  up: 'https://res.cloudinary.com/udemic/image/upload/v1632935479/hackmamba-emoji/up.png',
+  wow: 'https://res.cloudinary.com/udemic/image/upload/v1632935437/hackmamba-emoji/wow.png',
+  yay: 'https://res.cloudinary.com/udemic/image/upload/v1632935235/hackmamba-emoji/yay.png',
+};
 
 const emojiReactions = [
   {
-    content: "a",
-    time: 2
+    content: 'love',
+    time: 2,
   },
   {
-    content: "b",
-    time: 4
+    content: 'down',
+    time: 3,
   },
   {
-    content: "c",
-    time: 6
+    content: 'love',
+    time: 4,
   },
   {
-    content: "d",
-    time: 8
-  }
+    content: 'joy',
+    time: 5,
+  },
+  {
+    content: 'up',
+    time: 6,
+  },
+  {
+    content: 'wow',
+    time: 8,
+  },
+  {
+    content: 'yay',
+    time: 9,
+  },
+  {
+    content: 'up',
+    time: 11,
+  },
+  {
+    content: 'up',
+    time: 15,
+  },
 ];
 
 function VideoPlayerFunction(props) {
@@ -31,24 +60,30 @@ function VideoPlayerFunction(props) {
   const [playerDuration, setPlayerDuration] = useState(null);
 
   const [reactions, setReactions] = useState(emojiReactions);
-  const [liveReactions, setLiveReactions] = useState([
-    { content: "hey", time: 0 }
-  ]);
+  const [liveReactions, setLiveReactions] = useState([]);
 
   // keep track of whether video control is visible
   const [userActive, setUserActive] = useState(false);
 
   const cld = new Cloudinary({
     cloud_name: props.options.cloudName,
-    secure: true
+    secure: true,
   });
 
   // setup an event handler to be called at every second of the video
   const runPerVideoSecond = (videoPlayerObj, func) => {
     let currentIntTime = 0;
 
+    // BUG: currentIntTime does not update on video seek...
+    // TODO: create an event handler for the 'seek' event to reset the currentIntTime on seek
+    // Probable add this as flaws in the article that are fixed in the github repo version to keep the article simple
+
+    videoPlayerObj.on('seeked', () => {
+      currentIntTime = parseInt(videoPlayerObj.currentTime());
+    });
+
     // leaky abstraction
-    videoPlayerObj.on("timeupdate", () => {
+    videoPlayerObj.on('timeupdate', () => {
       if (parseInt(videoPlayerObj.currentTime()) > currentIntTime) {
         currentIntTime++;
         func(currentIntTime);
@@ -57,34 +92,40 @@ function VideoPlayerFunction(props) {
   };
 
   function videoPlayerInit() {
-    const player = cld.videoPlayer("example-player", {
+    const player = cld.videoPlayer('example-player', {
       controls: true,
-      // bigPlayButton: false,
-      playbackRates: ["0.5", "1.0", "1.25", "1.5", "1.75", "2.0"],
-      showLogo: true,
       colors: {
-        accent: "f7bc00"
+        accent: 'f7bc00',
       },
-      transformation: { fetch_format: "auto", quality: "auto" },
       fluid: true,
       showJumpControls: true,
-      floatingWhenNotVisible: "left"
     });
 
     player.source(
-      "https://res.cloudinary.com/greenpeg/video/upload/v1590960158/samples/sea-turtle.mp4"
+      'https://res.cloudinary.com/greenpeg/video/upload/v1590960158/samples/sea-turtle.mp4'
     );
 
     // make video player object available to function component scope after initialization
     setPlayerObj(player);
 
+    // WRITING: I could organize the article to highlight all the useful events we'd be watching for and why
+
     // safely get video duration when metadata has loaded
-    player.videojs.on("loadedmetadata", () =>
+    player.videojs.on('loadedmetadata', () =>
       setPlayerDuration(player.duration())
     );
 
+    // set userActive to true when video is first played
+    player.videojs.one('play', () => {
+      setUserActive(true);
+    });
+
     // useractive event(from videojs) checks for when the video controls are shown and hidden
-    player.videojs.on("useractive", () => {
+    player.videojs.on('useractive', () => {
+      setUserActive(player.videojs.userActive());
+    });
+
+    player.videojs.on('userinactive', () => {
       setUserActive(player.videojs.userActive());
     });
 
@@ -95,24 +136,21 @@ function VideoPlayerFunction(props) {
       reactions.forEach((rxn) => {
         if (rxn.time < currentIntTime) {
           liveReactionsList.push(rxn);
-          console.log("!pushed");
         }
 
         // make the latest reaction(at current time) 'pop'
         if (rxn.time === currentIntTime) {
-          rxn.active = true;
-          liveReactionsList.push(rxn);
+          // make copy of current active rxn and make its active key = true
+          let activeRxn = { ...rxn };
+          activeRxn.active = true;
+          liveReactionsList.push(activeRxn);
         }
       });
 
-      // troubleshooting: state is being updated
-      // I have a feeling this whole thing has something
-      // to do with react portals
       setLiveReactions(liveReactionsList);
 
-      // troubleshooting: referencing stale value?? yes
-      // but that isn't quite the issue here
-      console.log("live reactions list:", liveReactions);
+      // console.log('displayed reactions: ', { liveReactionsList });
+      console.log('current time: ', currentIntTime);
     });
 
     // make player object global so it can be inspected
@@ -124,14 +162,10 @@ function VideoPlayerFunction(props) {
   }, []);
 
   useEffect(() => {
-    console.log("liveReactions updating... ", liveReactions);
-  }, [liveReactions]);
-
-  useEffect(() => {
     // setIsMounted(true); // may not be necessary but let's watch!
-    const videoParent = document.querySelector(".cld-video-player");
+    const videoParent = document.querySelector('.cld-video-player');
     const videoControls = document.querySelector(
-      ".cld-video-player .vjs-control-bar"
+      '.cld-video-player .vjs-control-bar'
     );
 
     setVideoParentNode(videoParent);
@@ -146,19 +180,43 @@ function VideoPlayerFunction(props) {
           videoParentNode={videoParentNode}
           videoControlsNode={videoControlsNode}
         >
-          {liveReactions.map((rxn) => {
-            // the rerender is receiving the latest state value
-            console.log("live reactions render value --> ", liveReactions);
-            return (
-              <div
-                className="emoji-timeline-reaction"
-                style={{ left: `${(rxn.time / playerObj.duration()) * 100}%` }}
-                // style={{ color: "red" }}
-              >
-                {rxn.content}
-              </div>
-            );
-          })}
+          {userActive
+            ? reactions.map((rxn) => {
+                // make the 'emoji-timeline-reaction' element into a react component...
+
+                return (
+                  <div
+                    className={`emoji-timeline-reaction control-visible `}
+                    style={{
+                      left: `${(rxn.time / playerObj.duration()) * 100}%`,
+                    }}
+                  >
+                    <img
+                      src={emojiURLs[rxn.content]}
+                      alt={rxn.content}
+                      width="16px"
+                    />
+                  </div>
+                );
+              })
+            : liveReactions.map((rxn) => {
+                return (
+                  <div
+                    className={`emoji-timeline-reaction ${
+                      rxn.active ? 'active' : ''
+                    } `}
+                    style={{
+                      left: `${(rxn.time / playerObj.duration()) * 100}%`,
+                    }}
+                  >
+                    <img
+                      src={emojiURLs[rxn.content]}
+                      alt={rxn.content}
+                      width="16px"
+                    />
+                  </div>
+                );
+              })}
         </EmojiTimeline>
       )}
     </>
@@ -166,19 +224,10 @@ function VideoPlayerFunction(props) {
 }
 
 function EmojiTimeline({ videoParentNode, videoControlsNode, children }) {
-  const el = document.createElement("div");
-  el.className = "emoji-timeline";
+  const el = document.createElement('div');
+  el.className = 'emoji-timeline';
 
-  console.log("###=>", "portal renderer!", { children });
-
-  useEffect(() => {
-    videoParentNode.insertBefore(el, videoControlsNode);
-    console.log("--> running effect!");
-
-    return () => videoParentNode.removeChild(el);
-  }, []);
-
-  return ReactDOM.createPortal(children, el);
+  return <div className="emoji-timeline">{children}</div>;
 }
 
 export default VideoPlayerFunction;
